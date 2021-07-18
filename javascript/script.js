ee817a95aeb56dcc070bb895d5b70f2c16c298a5
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-07-18 15:47:21 -0300

    proximo comit

--- javascript/script.js
+++ javascript/script.js
@@ -3,10 +3,16 @@
 //Pegar todos os botões que existe com a classe check
 const checkButtons = document.querySelectorAll(".action a.check")
 
+const deleteButons = document.querySelectorAll(".action a.delete")
+
+const modalTitle = document.querySelector(".modal h2")
+
 checkButtons.forEach(botoes =>{
-    botoes.addEventListener("click", Event => {
-        abrir()
-    })
+    botoes.addEventListener("click", handleClick)
+})
+
+deleteButons.forEach(botoes =>{
+    botoes.addEventListener("click",(Event) => handleClick(Event, false))
 })
 
 
@@ -16,4 +22,11 @@ function abrir(){
 
 function fechar(){
     document.querySelector('.modal-wrapper').classList.remove("active")
+}
+
+function handleClick(Event, check = true){
+    modalTitle.innerHTML = check ? "Marcar como lida" : "Excluir essa pergunta"
+
+    //abrir modal
+    abrir()
 }
